c68322e02cfd13bed1183156cf21613eddd7703d
Dmitry Kalinin <demetryf@proton.me>
2024-09-03 23:49:05 +0300

    app: separate ctx from `AppState`

--- app/src/app/elements_panel.rs
+++ app/src/app/elements_panel.rs
@@ -1,79 +1,69 @@
 use egui::{Button, Id, Vec2};
 
+use super::Adding;
+use super::{AppState, Context};
 use crate::utils::WidgetsGallery;
-
-use super::{field::Adding, state::AppState};
 
 #[derive(Default)]
 pub struct ElementsPanel;
 
 impl ElementsPanel {
-    pub fn show<'app, 'data>(&mut self, state: &mut AppState<'app, 'data>) {
-        let screen_rect = state.ctx.screen_rect();
+    pub fn show(&mut self, state: &mut AppState, ctx: Context) {
+        let screen_rect = ctx.0.screen_rect();
 
         let max_panel_size = (screen_rect.height() - screen_rect.width()).abs();
 
         let min_coord = f32::min(screen_rect.width(), screen_rect.height());
 
         let max_size = Vec2::splat(min_coord / 8.0);
-        let min_size = Vec2::splat(min_coord / 12.0);
 
-        if state.ctx.screen_rect().width() > state.ctx.screen_rect().height() {
+        if ctx.0.screen_rect().width() > ctx.0.screen_rect().height() {
             egui::SidePanel::right(Id::new("elements_panel"))
                 .resizable(true)
                 .max_width(max_panel_size)
-                .show(state.ctx, |ui| {
-                    self.element_buttons(state, ui, max_size, min_size)
+                .show(ctx.0, |ui| {
+                    WidgetsGallery {
+                        max_size,
+                        widgets: self.buttons(state),
+                    }
+                    .show(ui)
                 });
         } else {
-            egui::TopBottomPanel::bottom(Id::new("id"))
+            egui::TopBottomPanel::bottom(Id::new("elements_panel"))
                 .resizable(true)
                 .max_height(max_panel_size)
-                .show(state.ctx, |ui| {
-                    self.element_buttons(state, ui, max_size, min_size)
+                .show(ctx.0, |ui| {
+                    WidgetsGallery {
+                        max_size,
+                        widgets: self.buttons(state),
+                    }
+                    .show(ui)
                 });
         }
     }
 
-    fn element_buttons(
-        &mut self,
-        state: &AppState,
-        ui: &mut egui::Ui,
-        max_size: Vec2,
-        min_size: Vec2,
-    ) {
-        let buttons = vec![
-            (Button::new("wire").min_size(min_size), ElementType::Wire),
-            (
-                Button::new("resistor").min_size(min_size),
-                ElementType::Resistor,
-            ),
-            (
-                Button::new("current source").min_size(min_size),
-                ElementType::CurrentSource,
-            ),
-        ];
-
+    fn buttons<'frame>(
+        &'frame mut self,
+        state: &'frame AppState,
+    ) -> impl Iterator<Item = (Button, Box<dyn FnOnce(egui::Response) + '_>)> + '_ {
         let adding = &state.adding;
 
-        let buttons = buttons.into_iter().map(|(button, element)| {
+        let buttons = [
+            (Button::new("wire"), ElementType::Wire),
+            (Button::new("resistor"), ElementType::Resistor),
+            (Button::new("current source"), ElementType::CurrentSource),
+        ]
+        .into_iter();
+
+        buttons.map(move |(button, ty)| {
             let react = Box::new(move |response: egui::Response| {
                 if response.clicked() {
-                    adding.set(Some(Adding {
-                        element,
-                        first: None,
-                    }))
+                    adding.set(Some(Adding::new(ty)));
                 }
             });
 
             (button, react as Box<dyn FnOnce(egui::Response)>)
-        });
-
-        WidgetsGallery {
-            max_size,
-            widgets: buttons,
-        }
-        .show(ui);
+        })
     }
 }
 
